1faf5a8d06253bca3ffcc1fd33f48d1428a9f5a0
pinargoktepe <pinargoktepe@gmail.com>
2019-05-05 18:32:36 +0200

    add device

--- train_eval.py
+++ train_eval.py
@@ -1,12 +1,14 @@
 import numpy as np
 import torch
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-def train(model, train_loader, optimizer, loss_fn, saveWeights, saving_path, loadWeights, loading_path, print_every=100):
+
+def train(model, train_loader, optimizer, loss_fn, saveWeights, saving_path, loadWeights, loading_path, device, print_every=100):
 
     if loadWeights:
         model.load_state_dict(torch.load(loading_path), strict=False)
+        model.to(device)
+        print("model weights are loaded from ", device)
 
     model.train()
 
@@ -27,13 +29,13 @@ def train(model, train_loader, optimizer, loss_fn, saveWeights, saving_path, loa
     accuracy = 100.0 * n_correct / len(train_loader.dataset)
 
     if saveWeights:
-        print("model will be saved")
+        print("Model weights are saved on ", device)
         torch.save(model.state_dict(), saving_path)
 
     return np.mean(np.array(losses)), accuracy
 
 
-def test(model, test_loader, loss_fn):
+def test(model, test_loader, loss_fn, device):
 
     model.eval()
     test_loss = 0
@@ -52,14 +54,14 @@ def test(model, test_loader, loss_fn):
     return average_loss, accuracy
 
 
-def fit(train_dataloader, val_dataloader, model, optimizer, loss_fn, n_epochs, scheduler=None, saveWeights=False, saving_path=None, loadWeights=False, loading_path=None):
+def fit(train_dataloader, val_dataloader, model, optimizer, loss_fn, n_epochs, device, scheduler=None, saveWeights=False, saving_path=None, loadWeights=False, loading_path=None):
     train_losses, train_accuracies = [], []
     val_losses, val_accuracies = [], []
 
     for epoch in range(n_epochs):
         print("epoch: ", epoch)
-        train_loss, train_accuracy = train(model, train_dataloader, optimizer, loss_fn, saveWeights, saving_path, loadWeights, loading_path)
-        val_loss, val_accuracy = test(model, val_dataloader, loss_fn)
+        train_loss, train_accuracy = train(model, train_dataloader, optimizer, loss_fn, saveWeights, saving_path, loadWeights, loading_path, device)
+        val_loss, val_accuracy = test(model, val_dataloader, loss_fn, device)
         train_losses.append(train_loss)
         train_accuracies.append(train_accuracy)
         val_losses.append(val_loss)
